10635faecf7034abe919b5a0deee0971672ca6a9
Samuel L.C <slcardoso.docente@gmail.com>
2024-11-15 10:18:02 +0100

    documentacion cookies

--- UD3/ejemplos/cookies_arrays.php
+++ UD3/ejemplos/cookies_arrays.php
@@ -1,12 +1,17 @@
 <?php
     $array = array('manzana','pera','cereza');
     if(isset($_COOKIE['frutas'])){
+        //Así lo haríamos directamente con el array.
         // $array = explode(":",$_COOKIE['frutas']);
+
+        //De esta forma pasamos los datos a json. Esto también podemos hacerlo con objetos.
         $array = json_decode($_COOKIE['frutas'],true);
 
     }
-    
+    //Asi lo haríamos directamente con el array.
     // $array_string = implode(":",$array);
+
+    //De esta forma pasamos los datos a json. Esto también podemos hacerlo con objetos.
     $array_string = json_encode($array);
 
 
