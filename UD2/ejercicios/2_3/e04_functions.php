c112f42f32fe2b639f1b07063025e077679395e1
Samuel L.C <slcardoso.docente@gmail.com>
2024-11-15 10:02:03 +0100

    cookies 1

--- UD2/ejercicios/2_3/e04_functions.php
+++ UD2/ejercicios/2_3/e04_functions.php
@@ -26,6 +26,12 @@ function get_videojuegos_filtros($nombre, $plataforma, $genero, $anio, $limit, $
     //Como pueden venir parametros a NULL aceptamos que puedan ser null en ele WHERE
     //Esta es una técnica para permitir nulos directamente en la SQL.
     //Tambien podríamos hacerlo con varios IF a la hora de construir la query, pero requeriría más código.
+
+    if($order_by==1){
+        $order = 'nombre';
+    }else if ($order_by==2){
+        $order = 'plataforma';
+    }
     $sql = "SELECT id, nombre, plataforma, anio_lanzamiento AS  anio, genero 
     FROM videojuegos
     WHERE (:nombreN IS NULL OR nombre LIKE :nombre) 
@@ -41,12 +47,12 @@ function get_videojuegos_filtros($nombre, $plataforma, $genero, $anio, $limit, $
     $query->bindValue(':plataformaN', $plataforma);
     $query->bindValue(':plataforma', $plataforma . '%'); // El % se pone para incluir el wildcard en la cadena que será evaluada por la cláusula LIKE de SQL.
     $query->bindValue(':anioN', $anio);
-    $query->bindValue(':anio', $anio,PDO::PARAM_INT);
+    $query->bindValue(':anio', $anio, PDO::PARAM_INT);
     $query->bindValue(':generoN', $genero);
     $query->bindValue(':genero', $genero) . '%';
     $query->bindValue(':lim', $limit, PDO::PARAM_INT);
     $query->bindValue(':of', $offset, PDO::PARAM_INT);
-    $query->bindValue(':orderby', $order_by,PDO::PARAM_INT);
+    $query->bindValue(':orderby', $order_by, PDO::PARAM_INT);
     $query->execute();
     $videojuegos = array();
     foreach ($query as $r) {
